d34cd006031afbd76b219c61396e904951ec68af
Manoj <manoj.khanwalkar@gmail.com>
2020-05-25 16:22:55 -0400

    Initial Commit

--- src/main/java/client/JobSubmitter.java
+++ src/main/java/client/JobSubmitter.java
@@ -1,10 +1,7 @@
 package client;
 
 import com.amazonaws.services.batch.model.SubmitJobRequest;
-import data.StatusRequest;
-import data.StatusResponse;
-import data.SubmitRequest;
-import data.SubmitResponse;
+import data.*;
 import org.apache.commons.codec.binary.Base64;
 import util.Connection;
 import util.JSONUtil;
@@ -12,6 +9,12 @@ import util.JarUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class JobSubmitter {
 
@@ -27,31 +30,44 @@ public class JobSubmitter {
 
     public void test()
     {
-        String jar = JarUtils.getBase64EncodedStringFromJar("/home/manoj/data/jabber/epi.jar");
-        SubmitRequest request = new SubmitRequest(jar, "client1","tmp.Test");
-
-        SubmitResponse response = submitJob(request);
-
-        System.out.println(response);
-
         ArrayList<String> jobids = new ArrayList<>();
-        jobids.add(response.getId());
 
-        StatusRequest statusRequest = new StatusRequest(jobids,"client1");
-        StatusResponse statusResponse = getStatus(statusRequest);
 
-        System.out.println(statusResponse);
+        for (int i=0;i<10;i++) {
+            String jar = JarUtils.getBase64EncodedStringFromJar("/home/manoj/data/jabber/epi.jar");
+            SubmitRequest request = new SubmitRequest(jar, "client1", "tmp.Test");
+            SubmitResponse response = submitJob(request);
+
+            System.out.println(response);
+            jobids.add(response.getId());
+
+        }
+
+        for (int i=0;i<10;i++) {
+            String jar = JarUtils.getBase64EncodedStringFromJar("/home/manoj/data/jabber/epi.jar");
+            SubmitRequest request = new SubmitRequest(jar, "client1", "tmp.AnotherTest");
+            SubmitResponse response = submitJob(request);
+
+            System.out.println(response);
+            jobids.add(response.getId());
+
+        }
+
+
+
+        Lock lock = new ReentrantLock();
+
+        Condition responseRecd = lock.newCondition();
+
+        CompletableFuture.runAsync(new StatusTracker(jobids, responseRecd,lock));
 
         try {
-            Thread.sleep(7000);
+            lock.lock();
+            responseRecd.await();
+            lock.unlock();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        statusResponse = getStatus(statusRequest);
-
-        System.out.println(statusResponse);
-
 
     }
 
@@ -73,15 +89,73 @@ public class JobSubmitter {
     }
 
 
-    private StatusResponse getStatus(StatusRequest statusRequest)
+
+
+    static class StatusTracker implements Runnable
     {
-        Connection app = new Connection("https://localhost:8480/");
+        volatile ArrayList<String> jobids;
+        Condition responseRecd;
+        Lock lock;
 
-        String respStr =  app.sendSimple(JSONUtil.toJSON(statusRequest),"status");
+        public StatusTracker( ArrayList<String> jobids, Condition responseRecd, Lock lock)
+        {
+            this.jobids = jobids;
+            this.responseRecd= responseRecd;
+            this.lock = lock;
+        }
 
-        StatusResponse response = (StatusResponse)JSONUtil.fromJSON(respStr,StatusResponse.class);
+        private StatusResponse getStatus(StatusRequest statusRequest)
+        {
+            Connection app = new Connection("https://localhost:8480/");
 
-        return response;
+            String respStr =  app.sendSimple(JSONUtil.toJSON(statusRequest),"status");
+
+            StatusResponse response = (StatusResponse)JSONUtil.fromJSON(respStr,StatusResponse.class);
+
+            return response;
+        }
+
+        private void processResponse(StatusResponse statusResponse)
+        {
+            statusResponse.getJobStatus().stream().forEach(st->{
+
+                Result result = st.getResult();
+                if (result!=null)
+                {
+                    jobids.remove(st.getId());
+                }
+            });
+
+            if (!jobids.isEmpty())
+            {
+                Future<Void> future = CompletableFuture.runAsync(new StatusTracker(jobids,responseRecd,lock));
+
+            }
+            else
+            {
+                lock.lock();
+                responseRecd.signalAll();
+                lock.unlock();
+
+            }
+
+        }
+
+        public void run()
+        {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            StatusRequest statusRequest = new StatusRequest(jobids,"client1");
+            StatusResponse statusResponse = getStatus(statusRequest);
+            System.out.println(statusResponse);
+
+            processResponse(statusResponse);
+
+        }
     }
 
 }
